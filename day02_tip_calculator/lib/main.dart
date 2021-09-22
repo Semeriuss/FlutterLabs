7ba36be2e7bfe0289179fca6811aac9d02c09021
Semere Habtu <se.semere.habtu@gmail.com>
2021-09-22 10:33:55 +0300

    tip calculator

---
+++ day02_tip_calculator/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = TextEditingController();
+
+  List<bool> _selection = [true, false, false];
+
+  String? tip;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (tip != null)
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    tip as String,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              Text('Total Amount'),
+              SizedBox(
+                width: 70,
+                child: TextField(
+                  controller: controller,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(hintText: '\$100.00'),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ToggleButtons(
+                  children: [Text('10%'), Text('15%'), Text('20%')],
+                  isSelected: _selection,
+                  onPressed: updateSelection,
+                ),
+              ),
+              TextButton(
+                child: Text(
+                  'Calculate Tip',
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: calculateTip,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void updateSelection(int selectedIndex) {
+    setState(() {
+      for (int i = 0; i < _selection.length; i++) {
+        _selection[i] = selectedIndex == i;
+      }
+    });
+  }
+
+  void calculateTip() {
+    final totalAmount = double.parse(controller.text);
+    final selectedIndex = _selection.indexWhere((element) => element);
+    final tipPercentage = [0.1, 0.15, 0.2][selectedIndex];
+
+    final tipTotal = (totalAmount * tipPercentage).toStringAsFixed(2);
+    setState(() {
+      tip = '\$$tipTotal';
+    });
+  }
+}
